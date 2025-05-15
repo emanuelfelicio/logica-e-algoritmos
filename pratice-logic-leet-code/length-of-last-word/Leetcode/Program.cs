71e39362e98c3ebf8a3b47aa9da14266a59d587e
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-05-15 18:21:08 -0300

    update

---
+++ pratice-logic-leet-code/length-of-last-word/Leetcode/Program.cs
@@ -0,0 +1,23 @@
+﻿
+public class Solution
+{
+    public static int LengthOfLastWord(string s)
+    {
+        int lastWordLength = 0;
+
+        s = s.Trim();
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] != ' ')
+            {
+                lastWordLength++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return lastWordLength;
+    }
+}
