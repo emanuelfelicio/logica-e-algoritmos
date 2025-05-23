ef3da82de88ee42b793f563f714f7c631dbd109c
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-05-23 11:14:55 -0300

    update

---
+++ pratice-logic-leet-code/climbing-stairs/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine(Solution.ClimbStairs(40));
+
+public static class Solution
+{
+    public static int ClimbStairs(int n)
+    {
+        return Fibbonacci(n);
+    }
+
+    private static int Fibbonacci(int n, Dictionary<int, int> dict = null)
+    {
+        dict ??= new Dictionary<int, int>();
+
+        if (n <= 1)
+        {
+            return 1;
+        }
+        if (dict.TryGetValue(n, out int value))
+        {
+            return value;
+        }
+        dict[n] = Fibbonacci(n - 1, dict) + Fibbonacci(n - 2, dict);
+        return dict[n];
+    }
+}
