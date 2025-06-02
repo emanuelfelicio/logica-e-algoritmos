58bd72b5bcd13fb55e581e7f3d3ec950bf6ac00c
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-06-02 19:22:56 -0300

    update

---
+++ pratice-logic-leet-code/3sum/Sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+public class Solution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        IList<IList<int>> result = [];
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i > 0 && nums[i - 1] == nums[i])
+            {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0)
+                {
+                    left++;
+                }
+                else if (sum > 0)
+                {
+                    right--;
+                }
+                else
+                {
+                    result.Add([nums[i], nums[left], nums[right]]);
+                    left++;
+                    while (nums[left] == nums[left-1] && left<right)
+                    {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return result;
+
+
+    }
+
+}
---
+++ study-recursion/fibbonacci-loop-way/Fibbonacci/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Diagnostics;
+
+Console.WriteLine(Fibonacci.LoopFibonacci(3));
+
+static class Fibonacci
+{
+    public static long RecursionFibonacci(uint n, Dictionary<uint, long> memo = null)
+    {
+        memo ??= [];
+
+        if (n == 0)
+        {
+            return 0;
+        }
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        if (memo.TryGetValue(n, out var value))
+        {
+            return value;
+        }
+
+        return memo[n] = RecursionFibonacci(n - 1, memo) + RecursionFibonacci(n - 2, memo);
+
+    }
+
+    public static long LoopFibonacci(uint n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        long prev = 0;
+        long curr = 1;
+
+        for (uint i = 2; i <= n; i++)
+        {
+            long next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+
+        return curr;
+    }
+
+}
