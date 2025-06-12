1de5d5d3731b9996c87fcaf5f8a71c400a919f50
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-06-12 18:07:08 -0300

    update

---
+++ pratice-logic-leet-code/binary-tree-inorder-traversal/InorderTraversal/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution
+{
+
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+
+        IList<int> result = [];
+        Stack<TreeNode> stack = [];
+        /*
+            RECURSIVE WAY
+
+            void RecursiveInorder(TreeNode root)
+            {
+                if (root == null) return;
+                RecursiveInorder(root.left);
+                result.Add(root.val);
+                RecursiveInorder(root.right);
+            }
+            RecursiveInorder(root);
+
+        */
+        TreeNode current = root;
+        while (current != null || stack.Count != 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+            current = stack.Pop();
+            result.Add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
+
+}
---
+++ pratice-logic-leet-code/restore-ip-addresses/LeetCode/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public IList<string> RestoreIpAddresses(string s)
+    {
+
+        List<string> result = new List<string>();
+
+        int len = s.Length;
+
+        if (len < 4 || len > 12)
+        {
+            return [];
+        }
+
+        for (int i = 1; i < 4 && i < len - 2; i++)
+        {
+            for (int j = i + 1; j < i + 4 && j < len - 1; j++)
+            {
+                for (int k = j + 1; k < j + 4 && k < len; k++)
+                {
+                    string part1 = s[..i];
+                    string part2 = s[i..j];
+                    string part3 = s[j..k];
+                    string part4 = s[k..];
+
+                    if (IsValid(part1) && IsValid(part2) && IsValid(part3) && IsValid(part4))
+                    {
+                        result.Add($"{part1}.{part2}.{part3}.{part4}");
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static bool IsValid(string s)
+    {
+        if (s.Length == 0 || s.Length > 3)
+            return false;
+        if (s.StartsWith("0") && s.Length > 1)
+            return false;
+
+        int value = int.Parse(s);
+        return value >= 0 && value <= 255;
+    }
+
+
+
+}
+
