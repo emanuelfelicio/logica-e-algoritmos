03b3c072597bd5977ae2bd1de339e99a34b21c0b
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-06-30 19:07:57 -0300

    up

---
+++ pratice-logic-hackerrank/kaprekar-numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+Result.kaprekarNumbers(0, 7777);
+class Result
+{
+
+    /*
+     * Complete the 'kaprekarNumbers' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER p
+     *  2. INTEGER q
+     */
+
+    public static void kaprekarNumbers(int p, int q)
+    {
+        StringBuilder sb = new();
+        for (int i = p; i <= q; i++)
+        {
+            if (isKaprekarNumber(i))
+            {
+                sb.Append($"{i} ");
+            }
+        }
+
+        if (sb.Length == 0)
+        {
+            System.Console.WriteLine("INVALID RANGE");
+
+        }
+        else
+        {
+            System.Console.WriteLine(sb);
+        }
+    }
+
+    public static bool isKaprekarNumber(int number)
+    {
+        string squareNumber = Math.Pow(number, 2).ToString();
+        string firstHalf = squareNumber[..(squareNumber.Length / 2)];
+        string secondeHalf = squareNumber[firstHalf.Length..];
+
+        int.TryParse(firstHalf, out int first);
+        int.TryParse(secondeHalf, out int second);
+
+        return first + second == number;
+    }
+
+}
---
+++ pratice-logic-leet-code/symmetric-tree/Program.cs
@@ -0,0 +1,54 @@
+﻿
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Solution
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        Queue<TreeNode> nodes = new();
+
+        if (root == null)
+        {
+            return true;
+        }
+
+        nodes.Enqueue(root.left);
+        nodes.Enqueue(root.right);
+
+        while (nodes.Count > 0)
+        {
+            TreeNode left = nodes.Dequeue();
+            TreeNode right = nodes.Dequeue();
+
+            if (left != null && right != null)
+            {
+                if (left.val != right.val)
+                {
+                    return false;
+                }
+                nodes.Enqueue(left.left);
+                nodes.Enqueue(right.right);
+                nodes.Enqueue(right.left);
+                nodes.Enqueue(left.right);
+
+            }
+            else if (!(left == null && right == null))
+            {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
